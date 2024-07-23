de078bb6ef7104f1d74cc3e17a8195fdcbe4465d
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-07-23 08:09:25 +0700

    a

--- src/pages/WinLossReport.jsx
+++ src/pages/WinLossReport.jsx
@@ -1,103 +1,155 @@
-import React from 'react'
-import { Badge, Button, ButtonGroup, Form } from 'react-bootstrap'
-import '../assets/css/history.css'
-import { DataGrid } from '@mui/x-data-grid';
-const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'gameName', headerName: 'Game Name', width: 130 },
-    { field: 'status', headerName: 'Status', width: 130 },
+import React, { useState } from "react";
+import { Badge, Button, ButtonGroup, Form } from "react-bootstrap";
+import "../assets/css/history.css";
+import { DataGrid } from "@mui/x-data-grid";
+import useFetch from "../hooks/useFetch";
+import BASE_URL from "../hooks/baseURL";
 
-    { field: 'openingBalance', headerName: 'Opening Balance', width: 130 },
-    {
-      field: 'closingBalance',
-      headerName: 'Closing Balance',
-      type: 'number',
-      width: 120,
-    },
-    {
-        field: 'betAmount',
-        headerName: 'Bet Amount',
-        type: 'number',
-        width: 120,
-      },
-      {
-        field: 'returnAmount',
-        headerName: 'Return Amount',
-        type: 'number',
-        width: 120,
-      },
-    {
-      field: 'date',
-      headerName: 'Date',
-    //   description: 'This column has a value getter and is not sortable.',
-    //   sortable: false,
-      width: 160,
-    //   valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-    },
-  ];
-  
-  const rows = [
-    { id: 1, gameName: 'Slot',status:'win', openingBalance: '10000', closingBalance: '20000',betAmount:10000,returnAmount:10000,date:new Date().toDateString() },
-    { id: 2, gameName: 'Slot',status:'win', openingBalance: '10000', closingBalance: '20000',betAmount:10000,returnAmount:10000,date:new Date().toDateString() },
-    { id: 3, gameName: 'Slot',status:'win', openingBalance: '10000', closingBalance: '20000',betAmount:10000,returnAmount:10000,date:new Date().toDateString() },
-    { id: 4, gameName: 'Slot',status:'loss', openingBalance: '10000', closingBalance: '20000',betAmount:10000,returnAmount:10000,date:new Date().toDateString() },
-    { id: 5, gameName: 'Slot', status:'win',openingBalance: '10000', closingBalance: '20000',betAmount:10000,returnAmount:10000,date:new Date().toDateString() },
-    { id: 6, gameName: 'Slot',status:'win', openingBalance: '10000', closingBalance: '20000',betAmount:10000,returnAmount:10000,date:new Date().toDateString() },
-   ];
-  
+const trans_columns = [
+  {
+    field: "id",
+    headerName: "No",
+    width: 150,
+  },
+  {
+    field: "closing_balance",
+    headerName: "Closing Balance",
+    width: 150,
+  },
+  {
+    field: "type",
+    headerName: "Type",
+    width: 150,
+  },
+  {
+    field: "amount",
+    headerName: "Amount",
+    width: 150,
+  },
+  { 
+    field: "datetime", 
+    headerName: "Date", 
+    width: 200 
+  },
+];
+
 const WinLossReportPage = () => {
+  const [show, setShow] = useState(false);
+
+  const [gParam, setGParam] = useState("today");
+  const [tParam, setTParam] = useState("today");
+
+  const { data: g_logs, loading: g_loading } = useFetch(
+    BASE_URL + "/wager-logs?type=" + gParam
+  );
+  const { data: t_logs, loading: t_loading } = useFetch(
+    BASE_URL + "/transactions?type=" + tParam
+  );
+
+  const language = localStorage.getItem("lan");
+
   return (
-    <div className='history pt-4 pb-5 px-3 px-sm-4' style={{overflowX:'hidden'}}>
-      <div className="row">
-      <Form.Group className='col-6 col-sm-4 pe-2 mt-3'  controlId="formBasicEmail">
-        <Form.Label>Start Date</Form.Label>
-        <Form.Control type="date"   />
-       </Form.Group>
-       <Form.Group  className='col-6 col-sm-4 pe-2 mt-3' controlId="formBasicEmail">
-        <Form.Label>End Date</Form.Label>
-        <Form.Control type="date"   />
-       </Form.Group>
-        <button  style={{height:'max-content'}} className="mx-auto mt-3 mt-sm-5  col-10 col-sm-4 col-xl-3 px-2 loginBtn py-1 px-3 rounded-3 fw-semibold">
-            <small >Show Results</small>
-        </button>
-      </div>
+    <div
+      className="history pt-4 pb-5 px-3 px-sm-4"
+      style={{ overflowX: "hidden" }}
+    >
+      <h5 className="fw-semibold text-white">
+        {language === "english" ? "Win/Loss Report" : "နိုင်/ရှုံး မှတ်တမ်း"}
+      </h5>
       <div className=" historyContainer mb-5  mt-4 p-3 rounded-3 ">
         <div className="mb-4 d-flex flex-wrap flex-sm-nowrap align-items-center gap-4">
-        <div style={{width:'max-content'}} className='py-1 px-2 rounded-3 border border-white d-flex flex-nowrap'>
-            <small className="fw-semibold historyTitle">All</small>
-            <Badge className='ms-2'  bg="secondary">
-                <small>6</small>
-            </Badge>
+          <button
+            className={`btn btn-${!show ? "light" : "outline-light"}`}
+            onClick={(e) => setShow(false)}
+          >
+            Game​ Logs
+          </button>
+          <button
+            className={`btn btn-${show ? "light" : "outline-light"}`}
+            onClick={(e) => setShow(true)}
+          >
+            Transaction Logs
+          </button>
         </div>
-      <ButtonGroup className="">
-        <Button style={{border:'none'}} className='bg-white text-black d-flex flex-nowrap '>
-            <small className="fw-semibold historyTitle">Win</small>
-            <Badge className='ms-1 ms-sm-2'  bg="secondary">
-                <small>8</small>
-            </Badge>
-        </Button>
-        <Button style={{border:'none'}} className='bg-white text-black  d-flex flex-nowrap'>
-            <small className="fw-semibold historyTitle">Loss</small>
-            <Badge className='ms-1 ms-sm-2' bg="secondary">
-                <small>5</small>
-            </Badge>
-        </Button>
-       </ButtonGroup>
-        </div>
-        <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
+        {!show && (
+          <div className="mb-4 d-flex gap-2">
+            <button className={`btn btn-sm btn-${gParam === 'today' ? "light" : "outline-light"}`} onClick={() => setGParam("today")}>
+              Today
+            </button>
+            <button className={`btn btn-sm btn-${gParam === 'yesterday' ? "light" : "outline-light"}`} onClick={() => setGParam("yesterday")}>
+              Yesterday
+            </button>
+            <button className={`btn btn-sm btn-${gParam === 'this_week' ? "light" : "outline-light"}`} onClick={() => setGParam("this_week")}>
+              This Week
+            </button>
+            <button className={`btn btn-sm btn-${gParam === 'last_week' ? "light" : "outline-light"}`} onClick={() => setGParam("last_week")}>
+              Last Week
+            </button>
+          </div>
+        )}
+        {show && (
+          <div className="mb-4 d-flex gap-2">
+            <button className={`btn btn-sm btn-${tParam === 'today' ? "light" : "outline-light"}`} onClick={() => setTParam("today")}>
+              Today
+            </button>
+            <button className={`btn btn-sm btn-${tParam === 'yesterday' ? "light" : "outline-light"}`} onClick={() => setTParam("yesterday")}>
+              Yesterday
+            </button>
+            <button className={`btn btn-sm btn-${tParam === 'this_week' ? "light" : "outline-light"}`} onClick={() => setTParam("this_week")}>
+              This Week
+            </button>
+            <button className={`btn btn-sm btn-${tParam === 'last_week' ? "light" : "outline-light"}`} onClick={() => setTParam("last_week")}>
+              Last Week
+            </button>
+          </div>
+        )}
+
+        {show && (
+          <DataGrid
+            rows={t_logs && t_logs}
+            columns={trans_columns}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+              },
+            }}
+            pageSizeOptions={[5, 10]}
+            checkboxSelection
+          />
+        )}
+        {!show && (
+          <div className="table-responsive">
+            <table className="table text-center">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>From</th>
+                  <th>To</th>
+                  <th>Game Provider</th>
+                  <th>Count</th>
+                  <th>Bet Amount</th>
+                  <th>Transaction Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {g_logs && g_logs.map((item, index) => (
+                  <tr key={index}>
+                    <td>{++index}</td>
+                    <td>{item.from_date}</td>
+                    <td>{item.to_date}</td>
+                    <td>{item.product}</td>
+                    <td>{item.total_count}</td>
+                    <td>{item.total_bet_amount}</td>
+                    <td>{item.total_transaction_amount}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WinLossReportPage
+export default WinLossReportPage;
